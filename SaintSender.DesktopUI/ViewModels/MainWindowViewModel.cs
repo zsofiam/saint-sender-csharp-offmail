c26a074d765275e89e0d2e2e1513b5f8ef849fe4
Farkas Timot <farkas.timot@gmail.com>
2021-08-12 09:50:00 +0200

    refactor: removed dead code from MainWindowViewModel

--- SaintSender.DesktopUI/ViewModels/MainWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -11,15 +11,17 @@ namespace SaintSender.DesktopUI.ViewModels
     /// </summary>
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-        //private string _name;
-        //private string _greeting;
-        //private readonly IGreetService _greetService;
         private readonly IUserService _userService;
 
         /// <summary>
         /// Whenever a property value changed the subscribed event handler is called.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            _userService = new UserService();
+        }
 
         internal void logout()
         {
@@ -31,39 +33,6 @@ namespace SaintSender.DesktopUI.ViewModels
             _userService.DeleteCredentials();
         }
 
-        /// <summary>
-        /// Gets or sets value of Greeting.
-        /// </summary>
-        /*public string Greeting
-        {
-            get { return _greeting; }
-            set
-            {
-                _greeting = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Greeting)));
-            }
-        }
-
-        /// <summary>
-        /// Gets or sets the name.
-        /// </summary>
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
-            }
-        }*/
-
-        public MainWindowViewModel()
-        {
-            //Name = string.Empty;
-            //_greetService = new GreetService();
-            _userService = new UserService();
-        }
-
         public bool AutoLogin()
         {
             return _userService.AutoLogin();
@@ -73,13 +42,5 @@ namespace SaintSender.DesktopUI.ViewModels
         {
             return _userService.IsLoggedIn();
         }
-
-        /// <summary>
-        /// Call a vendor service and apply its value into <see cref="Greeting"/> property.
-        /// </summary>
-        /*public void Greet()
-        {
-            Greeting = _greetService.Greet(Name);
-        }*/
     }
 }
