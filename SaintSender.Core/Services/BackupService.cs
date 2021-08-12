47200f30115bbdd7d48021b37fc735f310c66051
Farkas Timot <farkas.timot@gmail.com>
2021-08-12 19:17:52 +0200

    feat: created BackupService class

---
+++ SaintSender.Core/Services/BackupService.cs
@@ -0,0 +1,27 @@
+﻿using SaintSender.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintSender.Core.Services
+{
+    public class BackupService : IBackupService
+    {
+        public void SaveBackup(string address, IList<EmailInfo> emails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EmailInfo> LoadEmails(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBackup(string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
