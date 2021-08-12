46703f7d0398a8971180ee27fa3dfb318d7c71af
Farkas Timot <farkas.timot@gmail.com>
2021-08-12 12:13:03 +0200

    fix: fixed the text not showing up on the listview

--- SaintSender.Core/Services/EmailInfo.cs
+++ SaintSender.Core/Services/EmailInfo.cs
@@ -1,18 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SaintSender.Core.Services
 {
-    public class EmailInfo
+    public class EmailInfo : INotifyPropertyChanged
     {
         private string _sender;
         private DateTime _recieved;
         private string _subject;
         private string _body;
         private bool _read;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public String Sender
+        {
+            get { return _sender; }
+            set
+            {
+                _sender = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Sender)));
+            }
+        }
+        public DateTime Received
+        {
+            get { return _recieved; }
+            set
+            {
+                _recieved = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Received)));
+            }
+        }
+        public String Subject
+        {
+            get { return _subject; }
+            set
+            {
+                _subject = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Subject)));
+            }
+        }
+        public bool Read
+        {
+            get { return _read; }
+            set
+            {
+                _read = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Read)));
+            }
+        }
 
         public EmailInfo(string sender, DateTime received, string subject, string body, bool read)
         {
